efce6c83f99430c3089a542f32b995a9f2027ae7
savion210 <savion1300@gmail.com>
2025-12-30 01:59:41 -0500

    Update OneShotSoundEventChannel.cs
    missing namespace

--- Runtime/Core/Scripts/Events/Audio/OneShotSoundEventChannel.cs
+++ Runtime/Core/Scripts/Events/Audio/OneShotSoundEventChannel.cs
@@ -1,5 +1,6 @@
 using LonecraftGames.Toolkit.Core.Data;
 using LonecraftGames.Toolkit.Core.Events;
+using UnityEngine;
 namespace LonecraftGames.Toolkit.Core.Events
 {
     [CreateAssetMenu(fileName = "SoundEventChannel", menuName = "LonecraftGames/Events/Audio/OneShotSoundEventChannel")]
